9b191a9bba4f9d6665d361d0e348cead4422c0e2
osamahasanone <osamahasanone@gmail.com>
2022-09-16 20:21:12 +0200

    fix linting

--- app/newsletter/models.py
+++ app/newsletter/models.py
@@ -13,13 +13,14 @@ class Client(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+
 class SubscriptionAttempt(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     client = models.ForeignKey(Client, on_delete=models.PROTECT)
     secret_code = models.CharField(max_length=25, unique=True)
     valid_from = models.DateTimeField(auto_now_add=True)
     valid_to = models.DateTimeField()
-    succeed_at = models.BooleanField(null=True, blank= True)
+    succeed_at = models.BooleanField(null=True, blank=True)
 
     def _str__(self):
         return f"{self.client} - {self.secret_code}"
