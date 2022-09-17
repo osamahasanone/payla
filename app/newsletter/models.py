d2b39ee9f9a15e8b947944bcb8776cd0ca7171d0
osamahasanone <osamahasanone@gmail.com>
2022-09-17 14:14:44 +0200

    add Client Queryset

--- app/newsletter/models.py
+++ app/newsletter/models.py
@@ -7,10 +7,17 @@ from django.urls import reverse
 from django.utils import timezone
 
 
+class ClientQuerySet(models.QuerySet):
+    def subscribed(self) -> "ClientQuerySet":
+        return self.filter(subscribed=True)
+
+
 class Client(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     subscribed = models.BooleanField(default=False, db_index=True)
+
+    objects = ClientQuerySet.as_manager()
 
     def __str__(self) -> str:
         return f"{self.user.first_name} {self.user.last_name}"
