64b751821263f0ee22b74025bc5899835bb81c70
osamahasanone <osamahasanone@gmail.com>
2022-09-18 14:19:42 +0200

    add swagger

--- app/server/urls.py
+++ app/server/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
+
+from core.views import read_docs
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("openapi", get_schema_view(title=""), name="openapi-schema"),
+    path("docs", read_docs, name="docs"),
     path("newsletter/", include("newsletter.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
